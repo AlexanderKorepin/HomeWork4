2fb1b6a1ffa1ce0484f51b11c1f2480251744556
Alexander <1987kerykery@mail.ru>
2023-02-11 13:00:02 +1100

    homework for the seminar 4

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumNumber(int number)
+{
+  int Sum = 0;
+  for (int current =1; current<=1; current++)
+  return Sum;
+}
+Console.WriteLine("Input number: ");
+int newnumber = Convert.ToInt32(Console.ReadLine());
+int result = SumNumberNunber(newnumber);
+Console.WriteLine($"Sum of elements form 1 to {newnumber} is {result}");
